470dca01ad6ee819dc545d885cae970a23370a0a
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-25 16:27:19 -0500

    Install & configure NGXS

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { NgModule } from '@angular/core';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+import { stateList } from '../store/state-list';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgxsModule.forRoot([...stateList], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production,
+    }),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production,
+    }),
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
