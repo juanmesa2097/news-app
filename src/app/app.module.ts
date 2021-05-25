465456e8b69edc8ec7a990e257f4f0b201ed8756
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-25 16:12:17 -0500

    Install & configure TaigaUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './components/layout/layout.module';
@@ -7,7 +9,14 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    TuiRootModule,
+    LayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
