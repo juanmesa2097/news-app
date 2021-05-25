4b66aaaf4d270fe101cc63d159187967a30271fb
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-25 15:45:58 -0500

    Create page wrappers components

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PageContentWrapperModule } from 'src/app/components/page-content-wrapper/page-content-wrapper.module';
+import { PageHeaderWrapperModule } from 'src/app/components/page-header-wrapper/page-header-wrapper.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
 @NgModule({
-  declarations: [
-    HomePage
+  declarations: [HomePage],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PageHeaderWrapperModule,
+    PageContentWrapperModule,
   ],
-  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
