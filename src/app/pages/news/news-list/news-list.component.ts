f17a01a90176d42a9ff042181c1bf77602fb44dc
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-26 00:33:47 -0500

    Update footer

--- src/app/pages/news/news-list/news-list.component.ts
+++ src/app/pages/news/news-list/news-list.component.ts
@@ -2,9 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnChanges,
   OnInit,
-  SimpleChanges,
 } from '@angular/core';
 import { News } from '@app/store/news/news.model';
 import { PathName } from 'src/app/core/enums/path-name.enum';
@@ -15,15 +13,12 @@ import { PathName } from 'src/app/core/enums/path-name.enum';
   styleUrls: ['./news-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewsListComponent implements OnInit, OnChanges {
+export class NewsListComponent implements OnInit {
   @Input() data!: News[];
 
   pathName = PathName;
 
   constructor() {}
-  ngOnChanges(changes: SimpleChanges): void {
-    this.data = [...this.data, ...this.data, ...this.data];
-  }
 
   ngOnInit(): void {}
 }
